371945c3a240a7312d8c9a8119e2518ede5eb5b8
Oded Shanoon <odeds@mellanox.com>
2014-01-16 15:21:01 +0200

    Author: Alfred Perlstein <alfred@freebsd.org> Date:   Mon Jan 14 02:21:58 2013 -0800
    mlx4_ib porting: Add additional typedefs needed to compile.
    
    Change-Id: I6e8c50b924a53b970e4605c6f57625794f747529
    Signed-off-by: Oded Shanoon <odeds@mellanox.com>

--- include/freebsd/include/asm/types.h
+++ include/freebsd/include/asm/types.h
@@ -33,9 +33,22 @@
 #ifdef _KERNEL
 
 typedef uint8_t u8;
+typedef uint8_t __u8;
 typedef uint16_t u16;
+typedef uint16_t __u16;
 typedef uint32_t u32;
+typedef uint32_t __u32;
 typedef uint64_t u64;
+typedef uint64_t __u64;
+
+typedef int8_t s8;
+typedef int8_t __s8;
+typedef int16_t s16;
+typedef int16_t __s16;
+typedef int32_t s32;
+typedef int32_t __s32;
+typedef int64_t s64;
+typedef int64_t __s64;
 
 /* DMA addresses come in generic and 64-bit flavours.  */
 typedef vm_paddr_t dma_addr_t;
