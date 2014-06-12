999c4c18849a4cc84ed0ce1d0de44babf323135d
Daria Genzel <dariaz@mellanox.com>
2014-06-12 19:18:36 +0300

    ib_core : umem.c compilation
    - Added function declaration at the top of the file.
    - Removed three contig pages related funcs currently not supported in
    FreeBSD. Oped a ticket to follow up.
    - Added hugetlb.h
    
    issue: 400415
    
    Change-Id: I35d92393ae1fef4f1c46c100e26d5753f12c62af
    Signed-off-by: Daria Genzel <dariaz@mellanox.com>

--- drivers/infiniband/core/umem.c
+++ drivers/infiniband/core/umem.c
@@ -47,6 +47,8 @@ module_param_named(weak_ordering, allow_weak_ordering, int, 0444);
 MODULE_PARM_DESC(weak_ordering,  "Allow weak ordering for data registered memory");
 
 
+int ib_umem_map_to_vma(struct ib_umem *umem, struct vm_area_struct *vma);
+
 static void umem_vma_open(struct vm_area_struct *area)
 {
 	/* Implementation is to prevent high level from merging some
@@ -194,17 +196,6 @@ static void ib_cmem_release(struct kref *ref)
 
 }
 
-/**
- * ib_cmem_release_contiguous_pages - release memory allocated by
- *                                              ib_cmem_alloc_contiguous_pages.
- * @cmem: cmem struct to release
- */
-void ib_cmem_release_contiguous_pages(struct ib_cmem *cmem)
-{
-	kref_put(&cmem->refcount, ib_cmem_release);
-}
-EXPORT_SYMBOL(ib_cmem_release_contiguous_pages);
-
 static void cmem_vma_open(struct vm_area_struct *area)
 {
 	struct ib_cmem *ib_cmem;
@@ -228,176 +219,10 @@ static void cmem_vma_close(struct vm_area_struct *area)
 
 }
 
-
 static const struct vm_operations_struct cmem_contig_pages_vm_ops = {
 	.open = cmem_vma_open,
 	.close = cmem_vma_close
 };
-
-/**
- * ib_cmem_map_contiguous_pages_to_vma - map contiguous pages into VMA
- * @ib_cmem: cmem structure returned by ib_cmem_alloc_contiguous_pages
- * @vma: VMA to inject pages into.
- */
-int ib_cmem_map_contiguous_pages_to_vma(struct ib_cmem *ib_cmem,
-						struct vm_area_struct *vma)
-{
-
-	int ret;
-	unsigned long page_entry;
-	unsigned long ntotal_pages;
-	unsigned long ncontig_pages;
-	unsigned long total_size;
-	struct page *page;
-	unsigned long vma_entry_number = 0;
-	struct ib_cmem_block *ib_cmem_block = NULL;
-
-	total_size = vma->vm_end - vma->vm_start;
-	if (ib_cmem->length != total_size)
-		return -EINVAL;
-
-	if (total_size != PAGE_ALIGN(total_size)) {
-		WARN(1,
-		"ib_cmem_map: total size %lu not aligned to page size\n",
-		total_size);
-		return -EINVAL;
-	}
-
-	ntotal_pages = total_size >> PAGE_SHIFT;
-	ncontig_pages = 1 << ib_cmem->block_order;
-
-	list_for_each_entry(ib_cmem_block, &(ib_cmem->ib_cmem_block), list) {
-		page = ib_cmem_block->page;
-		for (page_entry = 0; page_entry < ncontig_pages; page_entry++) {
-			/* We reached end of vma - going out from both loops */
-			if (vma_entry_number >= ntotal_pages)
-				goto end;
-
-			ret = vm_insert_page(vma, vma->vm_start +
-				(vma_entry_number << PAGE_SHIFT), page);
-			if (ret < 0)
-				goto err_vm_insert;
-
-			vma_entry_number++;
-			page++;
-		}
-	}
-
-end:
-
-	/* We expect to have enough pages   */
-	if (vma_entry_number >= ntotal_pages) {
-		vma->vm_ops =  &cmem_contig_pages_vm_ops;
-		vma->vm_private_data = ib_cmem;
-		return 0;
-	}
-	/* Not expected but if we reached here
-	    not enough contiguous pages were registered
-	*/
-	ret = -EINVAL;
-
-err_vm_insert:
-
-	zap_vma_ptes(vma, vma->vm_start, total_size);
-	return ret;
-
-}
-EXPORT_SYMBOL(ib_cmem_map_contiguous_pages_to_vma);
-
-
-
-/**
- * ib_cmem_alloc_contiguous_pages - allocate contiguous pages
-*  @context: userspace context to allocate memory for
- * @total_size: total required size for that allocation.
- * @page_size_order: order of one contiguous page.
- */
-struct ib_cmem *ib_cmem_alloc_contiguous_pages(struct ib_ucontext *context,
-				unsigned long total_size,
-				unsigned long page_size_order)
-{
-	struct ib_cmem *cmem;
-	unsigned long ntotal_pages;
-	unsigned long ncontiguous_pages;
-	unsigned long ncontiguous_groups;
-	struct page *page;
-	int i;
-	int ncontiguous_pages_order;
-	struct ib_cmem_block *ib_cmem_block;
-	unsigned long locked;
-	unsigned long lock_limit;
-
-	if (page_size_order < PAGE_SHIFT || page_size_order > 31)
-		return ERR_PTR(-EINVAL);
-
-	cmem = kzalloc(sizeof *cmem, GFP_KERNEL);
-	if (!cmem)
-		return ERR_PTR(-ENOMEM);
-
-	kref_init(&cmem->refcount);
-	cmem->context   = context;
-	INIT_LIST_HEAD(&cmem->ib_cmem_block);
-
-	/* Total size is expected to be already page aligned -
-	    verifying anyway.
-	*/
-	ntotal_pages = PAGE_ALIGN(total_size) >> PAGE_SHIFT;
-	/* ib_cmem_alloc_contiguous_pages is called as part of mmap
-	with mm->mmap_sem held for writing.
-	No need to lock
-	*/
-	locked     = ntotal_pages + current->mm->pinned_vm;
-	lock_limit = rlimit(RLIMIT_MEMLOCK) >> PAGE_SHIFT;
-
-	if ((locked > lock_limit) && !capable(CAP_IPC_LOCK))
-		goto err_alloc;
-
-	/* How many contiguous pages do we need in 1 block */
-	ncontiguous_pages = (1 << page_size_order) >> PAGE_SHIFT;
-	ncontiguous_pages_order = ilog2(ncontiguous_pages);
-	ncontiguous_groups = (ntotal_pages >> ncontiguous_pages_order)  +
-		(!!(ntotal_pages & (ncontiguous_pages - 1)));
-	
-	/* Checking MAX_ORDER to prevent WARN via calling alloc_pages below */
-	if (ncontiguous_pages_order >= MAX_ORDER)
-		goto err_alloc;
-	/* we set block_order before starting allocation to prevent
-	   a leak in a failure flow in ib_cmem_release.
-	   cmem->length has at that step value 0 from kzalloc as expected */
-	cmem->block_order = ncontiguous_pages_order;
-	for (i = 0; i < ncontiguous_groups; i++) {
-		/* Allocating the managed entry */
-		ib_cmem_block = kmalloc(sizeof(struct ib_cmem_block),
-				GFP_KERNEL);
-		if (!ib_cmem_block)
-			goto err_alloc;
-
-		page =  alloc_pages(GFP_HIGHUSER | __GFP_ZERO | __GFP_COMP |
-					__GFP_NOWARN, ncontiguous_pages_order);
-		if (!page) {
-			kfree(ib_cmem_block);
-			/* We should deallocate previous succeeded allocatations
-			     if exists.
-			*/
-			goto err_alloc;
-		}
-
-		ib_cmem_block->page = page;
-		list_add_tail(&ib_cmem_block->list, &cmem->ib_cmem_block);
-	}
-
-	cmem->length = total_size;
-
-	current->mm->pinned_vm = locked;
-	return cmem;
-
-err_alloc:
-
-	ib_cmem_release_contiguous_pages(cmem);
-	return ERR_PTR(-ENOMEM);
-
-}
-EXPORT_SYMBOL(ib_cmem_alloc_contiguous_pages);
 
 static struct ib_umem *peer_umem_get(struct ib_peer_memory_client *ib_peer_mem,
 				       struct ib_umem *umem, unsigned long addr,
@@ -476,6 +301,7 @@ out:
 	kfree(umem);
 	return ERR_PTR(ret);
 }
+
 static void peer_umem_release(struct ib_umem *umem)
 {
 	struct ib_peer_memory_client *ib_peer_mem = umem->ib_peer_mem;
