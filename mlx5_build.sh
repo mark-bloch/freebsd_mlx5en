9f17c395fdc5185a361769270807d54303d70dde
Mark Bloch <markb@mellanox.com>
2015-07-22 08:11:26 +0300

    Changes needed for public use.
    Chnages to the code so it could be compiled and used outside of Mellanox.
    
    Signed-off-by: Mark Bloch <markb@mellanox.com>

--- mlx5_build.sh
+++ mlx5_build.sh
@@ -5,7 +5,6 @@ SRC="$1"
 [ -z "$SRC" ] && SRC="/usr/src"
 
 echo "The FreeBSD source tree is at $SRC ..."
-bmake -m $SRC/share/mk SYSDIR=$SRC/sys -C mlx5_modules $LRO generate
 bmake -m $SRC/share/mk SYSDIR=$SRC/sys -C mlx5_modules $LRO clean cleandepend
 bmake -m $SRC/share/mk SYSDIR=$SRC/sys -C mlx5_modules $LRO depend
 bmake -m $SRC/share/mk SYSDIR=$SRC/sys -C mlx5_modules $LRO all -j12
